782376b220082fe2eb42164371c80ab56aea550a
FranceyGuerin <49476573+FranceyGuerin@users.noreply.github.com>
2020-07-07 23:09:14 +1200

    Created working script

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -7,16 +7,9 @@ To get started with TDD, see the `README.md` file in your
 =end
 
 class ResistorColorDuo
-  COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
 
   def self.value(colors)
-    values = []
-    colors = colors.first(2)
-
-    for color in colors do
-      values << COLORS.index(color)
-    end
-
-    values.join.to_i
+    colors.first(2).map {|c| COLORS.index(c)}.join.to_i
   end
 end
